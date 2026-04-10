71bf58030a74be71a11128d805599c7c73a645d9
Dodi Aditya <dodiaridharma@gmail.com>
2026-04-10 13:44:03 +0800

    chore: include apps in workspace

--- apps/vue-3/src/App.vue
+++ apps/vue-3/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Button } from "@my-org/ui";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <HelloWorld />
+  <Button>Hello world</Button>
 </template>
